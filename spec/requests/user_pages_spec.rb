1f3691914b36ad592ed6f22e93c9e018d2035755
Paulo Leonardo Vieira Rodrigues <paulo@souprogramador.com>
2013-12-25 19:20:15 -0200

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "User pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title("#{base_title} | Sign up") }
+  end
+end
